5ecd6484daa52c0a7d017f52b392bf96466ae528
juyibai <marchhead31@gmail.com>
2019-09-22 04:13:55 -0400

    Implement the function of getting today's friends.

--- src/main/java/nes/welcome2church/controller/FriendController.java
+++ src/main/java/nes/welcome2church/controller/FriendController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.*;
 
 
 import javax.annotation.Resource;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/friends")
+@CrossOrigin(origins = "*")
 public class FriendController {
 
     @Resource(name = "friendRepository")
@@ -23,12 +25,16 @@ public class FriendController {
         return friend.get();
     }
 
+    @GetMapping("/today")
+    @ResponseBody
+    public List<Friend> getFriendByCurrentDate() {
+        List<Friend> friends = repository.getFriendsByCurrentDate();
+        return friends;
+    }
+
     @PostMapping
     public Friend insertFriend(@RequestBody Friend friend) {
         return repository.save(friend);
     }
-
-
-
 }
 
--- src/main/java/nes/welcome2church/repository/FriendRepository.java
+++ src/main/java/nes/welcome2church/repository/FriendRepository.java
@@ -2,11 +2,19 @@ package nes.welcome2church.repository;
 
 import nes.welcome2church.po.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 @Repository("friendRepository")
 public interface FriendRepository extends JpaRepository<Friend, String> {
+
+    @Query(value= "SELECT * FROM tbl_friend WHERE DATE(first_visit_date) =DATE(CURDATE())",
+            nativeQuery=true)
+    public List<Friend> getFriendsByCurrentDate();
+
+
 }
